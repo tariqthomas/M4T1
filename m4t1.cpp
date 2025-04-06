5f11f9b7d702e6d7ea210c4415e6834524329efd
tariqthomas <thomast0609@student.faytechcc.edu>
2025-04-06 19:07:21 +0000

    m4t1,cpp

---
+++ m4t1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int number= 0;
+
+    while (number < 5)
+    {
+
+        cout << "hello\n";
+        number++;
+    }
+    cout << "that's all!\n" ;
+    return 0;
+}
+ 
